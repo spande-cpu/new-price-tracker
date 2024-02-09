9e39a2314651c9da0d6fd20bdd317892fb3254b8
spande-cpu <shashwatpande101@gmail.com>
2024-02-09 13:49:35 +0000

    Fixed typos

--- code/1) Data Collection.R
+++ code/1) Data Collection.R
@@ -11,7 +11,7 @@ source("./code/helpers/get_bellway.R")
 source("./code/helpers/get_persimmon.R")
 
 # Get raw data
-barrat <- get_barratt() %>% 
+barratt <- get_barratt() %>% 
   mutate_at(vars(contains("price") | contains("rooms")), as.numeric)
 persimmon <- get_bellway() %>% 
   mutate_at(vars(contains("price") | contains("rooms")), as.numeric)
@@ -26,14 +26,15 @@ ons_params <- c(
 )
 
 # New data
-new <- bind_rows(barrat, persimmon, bellway) %>%
-  mutate(across(where(contains("price"))), as.numeric)
+new <- bind_rows(barratt, persimmon, bellway) %>%
   select(Date, Year, Month, url, developer, developement, address, postcode, rooms_min, 
          rooms_max, price_from, price_upto, all_of(ons_params)) %>%
-  filter(!is.na(price_from))
+  filter(!is.na(price_from)) %>% 
+  as_tibble()
 
 # Previous price 
-previous <- readRDS("./raw_data/new_build_prices.RDS") 
+previous <- readRDS("./raw_data/new_build_prices.RDS") %>%
+  mutate(Date = date(Date))
 
 # Full data frame
 df <- bind_rows(previous, new) %>%
@@ -49,6 +50,59 @@ df <- bind_rows(previous, new) %>%
          price_mean = ifelse(is.na(price_upto), price_from, (price_from+price_upto)/2), 
          rooms_min.f = factor(rooms_min))
 
+# Clean Data
+levels(df$rooms_min.f) <- c("1","2","3","4","5", "6")
+# Set factor variable for no. of rooms
+df$rooms_min.f <- plyr::mapvalues(
+  df$rooms_min.f, from = c("1", "2", "3", "4", "5", "6"), to = c("1","2","3","4+","4+","4+")
+)
+levels(df$rooms_min.f) <- c("1","2","3","4+")
+
+# Assign geo-codes to NA postcodes
+tmp <- filter(df, is.na(european_electoral_region)) %>%
+  separate(postcode, c("out", "inc"), sep = " ") %>%
+  mutate(out = ifelse(is.na(inc), "DA10", out)) 
+postcode_list <- lapply(tmp$out, function(x) PostcodesioR::random_postcode(x)$postcode)
+postcode_list[sapply(postcode_list, is.null)] <- NA
+tmp$postcode <- unlist(postcode_list)
+tmp <- select(tmp, colnames(df)) %>%
+  select(-c(european_electoral_region,
+            eastings,northings,country,nhs_ha,longitude,latitude,primary_care_trust,
+            lsoa,msoa,incode,outcode,parliamentary_constituency,admin_district,admin_ward,
+            ccg,nuts)) %>%
+  filter(!is.na(postcode))
+# Lookup postcodes
+postcodes.tmp <- data.frame()
+for (i in 1:length(tmp$postcode)) {
+  tmp2 <- PostcodesioR::postcode_lookup(tmp$postcode[i])
+  postcodes.tmp <- bind_rows(postcodes.tmp, tmp2)
+}
+tmp <- cbind(tmp, postcodes.tmp)
+tmp <- select(tmp, colnames(df))
+# Remove tmp developments from Data
+df <- filter(df, !developement %in% unique(tmp$developement))
+# Return approximate approximate geo-codes to data
+df <- bind_rows(df, tmp)
+
+data <- df %>% 
+  group_by(developer, rooms_min.f, developement, latitude, longitude) %>%
+  ### Compute change in % terms 
+  mutate(
+    change_from = (c(NA, diff(price_from))/price_from) * 100, 
+    change_mean = (c(NA, diff(price_mean))/price_mean) * 100, 
+    change_upto = (c(NA, diff(price_upto))/price_upto) * 100
+  ) 
+
+# Clear environment
+rm(
+  tmp,postcodes.tmp,previous,new,
+  barratt,bellway,persimmon,
+  df,ons_params,
+  get_barratt,get_bellway,
+  get_persimmon, postcode_list
+  )
+
 # Results
 write_rds(df, "./raw_data/new_build_prices.RDS")
+write_rds(data, "./processed_data/new_build_prices_clean.RDS")
 
