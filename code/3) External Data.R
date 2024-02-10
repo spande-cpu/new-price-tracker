4204758ca8dc03eb05538a8f005ca439a12d9c70
spande-cpu <shashwatpande101@gmail.com>
2024-02-10 10:29:47 +0000

    Get external data from Nationwide and ONS

---
+++ code/3) External Data.R
@@ -0,0 +1,35 @@
+# Dependencies
+library(httr)
+library(lubridate)
+
+# Run upstream processes
+source("./code/2) Modelling.R")
+
+# Get Nationwide Data
+GET("https://www.nationwidehousepriceindex.co.uk/download/uk-monthly-index",
+    write_disk(tf <- tempfile(fileext = ".xls")))
+nationwide <- readxl::read_xls(tf)
+nationwide_small <- nationwide %>% 
+  rename("date" = "...1", "nationwide_prices" = `Average House Price`, 
+         "nationwide_change" = `Monthly % Change (SA)`,
+         "annual_nwd_change" = `Year % Change`) %>%
+  dplyr::select(date, nationwide_prices, nationwide_change, annual_nwd_change) %>% 
+  filter(lubridate::year(date) >= 2022) %>%
+  mutate(date = lubridate::date(date))
+saveRDS(nationwide_small, paste0("./processed_data/nationwide_sample_data.rds"))
+saveRDS(nationwide, paste0("./processed_data/nationwide_full_data.rds"))
+
+# Get ONS Data
+current <- month(today() %m-% months(3))
+file <- glue::glue("house-price-index-data/UK-HPI-full-file-2023-{current}.csv")
+uk_hpi <- readr::read_csv(glue::glue("https://publicdata.landregistry.gov.uk/market-trend-data/{file}"))
+uk_hpi <- uk_hpi %>%
+  filter(RegionName == "United Kingdom") %>%
+  select(Date, ONSAveragePrice = AveragePrice, `ONS1m%Change` = `1m%Change`, 
+         `ONS12m%Change` = `12m%Change`,
+         ONSNewPrice = NewPrice, `ONSNew1m%Change`= `New1m%Change`,
+         `ONSNew12m%Change` = `New12m%Change`,
+         ONSNewVolume = NewSalesVolume, ONSVolume = OldSalesVolume) %>%
+  mutate(Date = as.Date(Date, "%d/%m/%Y"))
+saveRDS(uk_hpi, "./processed_data/UK-HPI.rds")
+
