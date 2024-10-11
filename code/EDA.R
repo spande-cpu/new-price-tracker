d42723d131ef072fa969dd37111255ea3613c141
spande-cpu <shashwatpande101@gmail.com>
2024-10-11 17:06:43 +0100

    Initial commit

---
+++ code/EDA.R
@@ -0,0 +1,25 @@
+# Depends
+pacman::p_load(tidyverse)
+
+# Data
+df <- read_rds("./processed_data/new_build_prices_clean.RDS") |> ungroup()
+
+df |> 
+  mutate(
+    Month = factor(
+      Month, levels = c(
+        months(
+          seq.Date(
+            from = date("2024-01-01"), date("2024-12-01"), by = "month"
+            ), abbreviate = FALSE
+          )
+      )
+    )
+  ) |>
+  ggplot(aes(factor(Year), price_mean)) +
+  facet_wrap(vars(Month), scales = "fixed") +
+  stat_summary(
+    fun.data = "mean_cl_boot", aes(col = developer),
+    position = position_dodge(.5)
+    ) +
+  scale_y_continuous(labels = scales::dollar_format(prefix = "£"))
